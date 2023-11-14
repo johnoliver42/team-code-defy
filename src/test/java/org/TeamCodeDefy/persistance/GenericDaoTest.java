2539f6d7c8eb0e165381d9f28c022308f613cbe6
John Oliver <41064733+johnoliver42@users.noreply.github.com>
2023-11-14 17:05:01 -0600

    Added initial unit test for getting a book by ID to test project setup.

--- src/test/java/org/TeamCodeDefy/persistance/GenericDaoTest.java
+++ src/test/java/org/TeamCodeDefy/persistance/GenericDaoTest.java
@@ -3,10 +3,16 @@ package org.TeamCodeDefy.persistance;
 import org.TeamCodeDefy.entities.Book;
 import org.TeamCodeDefy.entities.ReadingList;
 import org.TeamCodeDefy.util.UnitTestUtil;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.*;
+
+import static org.junit.Assert.assertEquals;
+
 
 class GenericDaoTest {
+
+    private final Logger logger = LogManager.getLogger(this.getClass());
 
     private GenericDao genericBookDao = new GenericDao(Book.class);
     private GenericDao genericReadingListDao = new GenericDao(ReadingList.class);
@@ -15,14 +21,15 @@ class GenericDaoTest {
 
     @BeforeEach
     void setUp() {
-
         UnitTestUtil.createTestDatabase();
         UnitTestUtil.createTestData();
-
     }
 
     @Test
     void getById() {
+        GenericDao<Book> bookDao = new GenericDao<>(Book.class);
+        Book book = bookDao.getById(1028);
+        assertEquals("Upgradable systemic software", book.getTitle());
     }
 
     @Test
