1fc91ed94f2b5f4a331663b8a11b92c0b5361d81
Kailyn Angelo <kangelo@madisoncollege.edu>
2023-11-18 13:01:23 -0600

    add average rating, retrieve isbn and join authors methods

--- src/main/java/org/TeamCodeDefy/googleBooksApi/GoogleBookConversion.java
+++ src/main/java/org/TeamCodeDefy/googleBooksApi/GoogleBookConversion.java
@@ -2,6 +2,8 @@ package org.TeamCodeDefy.googleBooksApi;
 
 import org.TeamCodeDefy.entities.Book;
 
+import java.util.List;
+
 /**
 * Conversion methods for Google Book Response
 * */
@@ -15,15 +17,38 @@ public class GoogleBookConversion {
         VolumeInfo bookInfo = googleBookResponse.getItems().get(0).getVolumeInfo();
 
         book.setTitle(bookInfo.getTitle());
-        book.setAuthor(String.join(";", bookInfo.getAuthors()));
-        // TODO book isbn - see ItemsItem
+        book.setAuthor(joinAuthors(bookInfo.getAuthors()));
+        book.setIsbn(Integer.parseInt(getIsbn(bookInfo.getIndustryIdentifiers())));
         book.setDescription(bookInfo.getDescription());
         book.setPublisher(bookInfo.getPublisher());
         book.setLanguage(bookInfo.getLanguage());
         book.setPageCount(bookInfo.getPageCount());
-        // TODO average rating?? not available?
         book.setThumbnailLink(bookInfo.getImageLinks().getThumbnail());
+        book.setAverageRating(bookInfo.getAverageRating());
 
         return book;
+    }
+
+    private String getIsbn(List<IndustryIdentifiersItem> industryIdentifiers) {
+//        "industryIdentifiers": [
+//        {"type": "ISBN_13", "identifier": "9781476735115" },
+//        {"type": "ISBN_10", "identifier": "1476735115"}]
+            String isbn13 = null;
+            String isbn10 = null;
+
+        for (IndustryIdentifiersItem identifier : industryIdentifiers) {
+            if ("ISBN_13".equals(identifier.getType())) {
+                isbn13 = identifier.getIdentifier();
+            } else if ("ISBN_10".equals(identifier.getType())) {
+                isbn10 = identifier.getIdentifier();
+            }
+        }
+
+        // Prefer ISBN-13 if available, otherwise use ISBN-10
+        return (isbn13 != null) ? isbn13 : isbn10;
+    }
+
+    private String joinAuthors(List<String> authors) {
+        return authors != null ? String.join(";", authors) : "";
     }
 }
