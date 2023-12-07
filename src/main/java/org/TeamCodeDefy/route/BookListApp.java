b14439a8aebbd2b59a212c5463e3fb82864130de
John Oliver <41064733+johnoliver42@users.noreply.github.com>
2023-12-07 05:09:56 -0600

    Added application/server class for Jax-rs

---
+++ src/main/java/org/TeamCodeDefy/route/BookListApp.java
@@ -0,0 +1,24 @@
+package org.TeamCodeDefy.route;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+
+/**
+ * The class declares root resource and provider classes as well as the root uri path
+ */
+@ApplicationPath("/")
+public class BookListApp extends Application {
+
+    /**
+     * The method returns a non-empty collection with classes, that must be included in the published JAX-RS application
+     */
+    @Override
+    public Set<Class<?>> getClasses() {
+        HashSet<Class<?>> routes = new HashSet<>();
+        routes.add(BookListRoutes.class);
+        return routes;
+    }
+}
